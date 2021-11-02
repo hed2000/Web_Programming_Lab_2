45495856ef0706967aae53cc47e9c256134c84f5
hed2000 <91254825+hed2000@users.noreply.github.com>
2021-11-02 17:01:09 +0000

    Update game.js

--- game.js
+++ game.js
@@ -188,7 +188,7 @@ function updateBees() { // update loop for game
 	moveBees();
 	//use a fixed update period
 	let period = document.getElementById("periodTimer").value; 
-	if (hits.innerHRML < 999) {
+	if (hits.innerHTML < 19) {
 		clearTimeout(updateTimer);
 	}
 	else {
